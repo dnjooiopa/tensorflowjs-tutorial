b8a4722ede2d415a382bac5c95572183df215e2d
dnjooiopa <dnjooiopa@gmail.com>
2020-01-14 21:50:27 +0700

    refactor tensor

--- part1/scripts.js
+++ part1/scripts.js
@@ -56,10 +56,8 @@ async function run() {
 
     // predict
     const xpred = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-    const ypred = model
-        .predict(tf.tensor(xpred))
-        .flatten()
-        .arraySync();
+    const ypredTensor = model.predict(tf.tensor(xpred));
+    const ypred = ypredTensor.flatten().arraySync();
 
     const dataPredict = {
         x: xpred,
