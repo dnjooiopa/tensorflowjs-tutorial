9255d60b6ed6414730e0ed2d5f02d34a0c01be91
dnjooiopa <dnjooiopa@gmail.com>
2020-01-18 13:04:45 +0700

    fixed tfjs-node

--- part2/app.js
+++ part2/app.js
@@ -1,15 +1,3 @@
-const tf = require("@tensorflow/tfjs-node")
+const tf = require('@tensorflow/tfjs');
 
-
-async function run(){
-
-    const model = tf.sequential();
-
-    model.add(tf.layers.dense({
-        units: 64,
-        inputShape: [1],
-        activation: "r"
-    }))
-
-
-}
+require("@tensorflow/tfjs-node")
