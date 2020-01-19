4aa53712755992b9426f27f7ab7d0d083ef48349
dnjooiopa <dnjooiopa@gmail.com>
2020-01-19 17:35:39 +0700

    edit plotly

--- part2/app.js
+++ part2/app.js
@@ -1,3 +1,83 @@
-const tf = require('@tensorflow/tfjs');
+const tf = require('@tensorflow/tfjs-node');
+const plt = require("nodeplotlib")
 
-require("@tensorflow/tfjs-node")
+
+async function run() {
+    // data for simple linear regression
+    const trainData = {
+        x: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+        y: [1.79, 3, 4.75, 9.2, 12.15, 10.75, 16.55, 15.45, 17.4, 20.15, 24.1, 26.85, 27, 26.05, 28.6, 33.9, 32.65, 34.4, 39.45, 39.3, 43.35]
+    };
+
+    const dataTrain = {
+        x: trainData.x,
+        y: trainData.y,
+        name: "train data",
+        mode: "markers",
+        type: "scatter"
+    };
+
+    // plt.plot([dataTrain], {
+    //     width: 700,
+    //     title: "Simple linear regression",
+    //     xaxis: {
+    //         zeroline: true
+    //     }
+    // });
+
+    // create model
+    const model = tf.sequential();
+
+    model.add(
+        tf.layers.dense({
+            units: 64,
+            inputShape: [1],
+            activation: "relu"
+        })
+    );
+
+    model.add(
+        tf.layers.dense({
+            units: 1,
+            activation: "linear"
+        })
+    );
+
+    const LEARNING_RATE = 0.005;
+    model.compile({
+        optimizer: tf.train.adam(LEARNING_RATE),
+        loss: "meanSquaredError"
+    });
+
+    // prepare data for training
+    const trainX = tf.tensor(trainData.x);
+    const trainY = tf.tensor(trainData.y);
+
+    // train model
+    await model.fit(trainX, trainY, {
+        epochs: 100
+    });
+
+    // predict
+    const xpred = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const ypredTensor = model.predict(tf.tensor(xpred));
+    const ypred = ypredTensor.flatten().arraySync();
+
+    const dataPredict = {
+        x: xpred,
+        y: ypred,
+        name: "model line",
+        mode: "lines+markers",
+        type: "scatter"
+    };
+
+    plt.plot([dataTrain, dataPredict], {
+        width: 700,
+        title: "Simple linear regression (after train)",
+        xaxis: {
+            zeroline: true
+        }
+    });
+}
+
+run();
